9cb792d2aace46f60f58575f9e653bc262179481
Jose Bieco <jo_bieco@hotmail.com>
2021-11-05 14:31:03 -0300

    feat(demo/user/UserController.kt): register user

--- src/main/kotlin/com/example/demo/user/UserController.kt
+++ src/main/kotlin/com/example/demo/user/UserController.kt
@@ -1,6 +1,8 @@
 package com.example.demo.user
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,4 +20,10 @@ class UserController(
      *      Delete -> /auth, method: DELETE
      *      Update -> /auth/{userId}, method: PUT { Name, Password, Email? }
      * */
+
+    @PostMapping
+    @RequestMapping("/register")
+    fun register(@RequestBody user: User): User {
+        return this.service.create(user);
+    }
 }
