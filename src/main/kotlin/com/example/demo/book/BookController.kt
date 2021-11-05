5aa4a32ccd03faac4ad9915d31e0084bf3190fa7
Jose Bieco <jo_bieco@hotmail.com>
2021-11-05 10:54:22 -0300

    feat(demo/book/BookController.kt): update and pagination on getAll

--- src/main/kotlin/com/example/demo/book/BookController.kt
+++ src/main/kotlin/com/example/demo/book/BookController.kt
@@ -1,6 +1,8 @@
 package com.example.demo.book
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -12,7 +14,18 @@ class BookController(
     val service: BookService
 ) {
     @GetMapping
-    fun getAllBooks(): MutableIterable<Book> = service.getAll()
+    fun getAllBooks(
+        @RequestParam(value = "page", defaultValue = "0") page: Int,
+        @RequestParam(value = "linesPerPage", defaultValue = "5") linesPerPage: Int,
+        @RequestParam(value = "direction", defaultValue = "ASC") direction: String,
+        @RequestParam(value = "orderBy", defaultValue = "id") orderBy: String
+    ): MutableIterable<Book> {
+        val pageRequest: PageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy)
+        return service.getAll(pageRequest)
+    }
+
+    @GetMapping("/{bookId}")
+    fun findOne(@PathVariable bookId: Long): Book = service.getOne(bookId)
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -23,5 +36,10 @@ class BookController(
     @DeleteMapping("/{bookId}")
     fun delete(@PathVariable bookId: Long) {
         return this.service.delete(bookId)
+    }
+
+    @PutMapping("/{bookId}")
+    fun update(@PathVariable bookId: Long, @RequestBody updateBook: Book): Book {
+        return this.service.update(bookId, updateBook)
     }
 }
