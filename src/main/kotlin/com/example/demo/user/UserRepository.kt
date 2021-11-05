19acd2cccdebbcd67756006c39af164d03320741
Jose Bieco <jo_bieco@hotmail.com>
2021-11-05 14:32:11 -0300

    feat(demo/user/UserRepository.kt): query to search email

--- src/main/kotlin/com/example/demo/user/UserRepository.kt
+++ src/main/kotlin/com/example/demo/user/UserRepository.kt
@@ -1,6 +1,11 @@
 package com.example.demo.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository: JpaRepository<User, Long> {
+
+    // Get the count for how many times the email is registered
+    @Query("SELECT COUNT(*) AS Same FROM users WHERE email LIKE CONCAT('%', :email, '%')", nativeQuery = true)
+    fun searchEmail(email: String?): Long
 }
