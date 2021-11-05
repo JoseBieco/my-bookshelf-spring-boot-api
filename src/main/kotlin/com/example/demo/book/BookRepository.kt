5fbeae49edecd40975484275694ba6ea548134c5
Jose Bieco <jo_bieco@hotmail.com>
2021-11-05 10:55:21 -0300

    feat(demo/book/BookRepository.kt): added pagination

--- src/main/kotlin/com/example/demo/book/BookRepository.kt
+++ src/main/kotlin/com/example/demo/book/BookRepository.kt
@@ -1,9 +1,14 @@
 package com.example.demo.book
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BookRepository: CrudRepository<Book, Long> {
+interface BookRepository: JpaRepository<Book, Long>{
 
+    @Query("SELECT a FROM Book a ")
+    fun find(pageable: Pageable): Page<Book>
 }
