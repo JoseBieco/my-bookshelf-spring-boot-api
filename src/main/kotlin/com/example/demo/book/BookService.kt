fe09979b06b7fab522b5e809e5ed00dce89673df
Jose Bieco <jo_bieco@hotmail.com>
2021-11-05 10:56:17 -0300

    feat(demo/book/BookService.kt): Added update and pagination on getAll

--- src/main/kotlin/com/example/demo/book/BookService.kt
+++ src/main/kotlin/com/example/demo/book/BookService.kt
@@ -1,14 +1,20 @@
 package com.example.demo.book
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class BookService(
     @Autowired
     val db: BookRepository
 ) {
-    fun getAll(): MutableIterable<Book> = db.findAll()
+    fun getAll(pageable: Pageable): Page<Book> {
+        return db.find(pageable)
+    }
 
     fun create(newBook: Book): Book {
         return this.db.save(Book(
@@ -24,5 +30,21 @@ class BookService(
 
     fun delete(id: Long) {
         return this.db.deleteById(id)
+    }
+
+    fun update(id: Long, book: Book): Book {
+        if(!this.db.existsById(id)) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "The entity with id $id does not exist")
+        }
+        //var updateBook = this.db.findById(id)
+
+        return this.db.save(book)
+    }
+
+    fun getOne(id: Long): Book {
+        if(!this.db.existsById(id)) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "The entity with id $id does not exist")
+        }
+        return this.db.getOne(id)
     }
 }
