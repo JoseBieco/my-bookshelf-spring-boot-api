badac33985642bd2a295978fd974d275e710bb00
Jose Bieco <jo_bieco@hotmail.com>
2021-11-06 18:02:56 -0300

    feat(demo/user/UserRepository.kt): added query to select user by email

--- src/main/kotlin/com/example/demo/user/UserRepository.kt
+++ src/main/kotlin/com/example/demo/user/UserRepository.kt
@@ -8,4 +8,11 @@ interface UserRepository: JpaRepository<User, Long> {
     // Get the count for how many times the email is registered
     @Query("SELECT COUNT(*) AS Same FROM users WHERE email LIKE CONCAT('%', :email, '%')", nativeQuery = true)
     fun searchEmail(email: String?): Long
+
+    /**
+     * @param email String
+     * @return User based on email
+     */
+    @Query("SELECT * FROM users WHERE email LIKE :email", nativeQuery = true)
+    fun getByEmail(email: String?): User
 }
