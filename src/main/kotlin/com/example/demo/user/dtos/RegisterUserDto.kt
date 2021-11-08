b4791ce99605cb8e2a575ae69f67e4ee45ba0d74
Jose Bieco <jo_bieco@hotmail.com>
2021-11-08 09:19:20 -0300

    feat(demo/user/dtos/RegisterUserDto.kt): register user dto with data validation

--- src/main/kotlin/com/example/demo/user/dtos/RegisterUserDto.kt
+++ src/main/kotlin/com/example/demo/user/dtos/RegisterUserDto.kt
@@ -1,4 +1,17 @@
 package com.example.demo.user.dtos
 
-class RegisterUserDto {
+import com.example.demo.interfaces.UserDataValidator
+
+class RegisterUserDto(
+    var name: String? = null,
+    var email: String? = null,
+    var password: String? = null,
+): UserDataValidator {
+
+    /**
+     * @return True if the email, password and name are not empty
+     */
+    fun validate(): Boolean {
+        return this.notEmpty(this.email) && this.notEmpty(this.password) && this.notEmpty(this.name)
+    }
 }
