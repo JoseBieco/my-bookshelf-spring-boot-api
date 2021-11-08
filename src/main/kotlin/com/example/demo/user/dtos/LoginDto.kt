3b19abbb4089bfdb8a62fe33ca395d029770f2f7
Jose Bieco <jo_bieco@hotmail.com>
2021-11-08 09:20:42 -0300

    feat(demo/user/dtos/LoginDto.kt): set UserDataValidator interface

--- src/main/kotlin/com/example/demo/user/dtos/LoginDto.kt
+++ src/main/kotlin/com/example/demo/user/dtos/LoginDto.kt
@@ -1,21 +1,11 @@
 package com.example.demo.user.dtos
 
+import com.example.demo.interfaces.UserDataValidator
+
 class LoginDto(
     val email: String? = null,
     val password: String? = null
-) {
-
-    /**
-     * @param data String
-     * @return True if the data is not empty, and false if it's null or empty
-     */
-    private fun notEmpty(data: String?): Boolean {
-        if (data != null) {
-            return data.trim().isNotEmpty()
-        }
-        return false
-    }
-
+): UserDataValidator {
     /**
      * @return True if the email and the password are not empty
      */
