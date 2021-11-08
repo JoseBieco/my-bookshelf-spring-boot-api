4c48390519f0241e66137a01ae90263221338b2e
Jose Bieco <jo_bieco@hotmail.com>
2021-11-08 09:39:31 -0300

    feat(demo/user/UserController.kt): user delete

--- src/main/kotlin/com/example/demo/user/UserController.kt
+++ src/main/kotlin/com/example/demo/user/UserController.kt
@@ -3,10 +3,7 @@ package com.example.demo.user
 import com.example.demo.user.dtos.LoginDto
 import com.example.demo.user.dtos.RegisterUserDto
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("auth")
@@ -20,18 +17,24 @@ class UserController(
      *      Register -> /auth/register, method: POST
      *  Basic:
      *      Delete -> /auth, method: DELETE
-     *      Update -> /auth/{userId}, method: PUT { Name, Password, Email? }
+     *      Update -> /auth/{userId}, method: PUT { Name, Password, Email?; }
      * */
 
     @PostMapping
     @RequestMapping("/register")
     fun register(@RequestBody user: RegisterUserDto): User {
-        return this.service.create(user);
+        return this.service.create(user)
     }
 
     @PostMapping
     @RequestMapping("/login")
     fun login(@RequestBody login: LoginDto): User {
         return this.service.login(login)
+    }
+
+    @DeleteMapping
+    @RequestMapping("/{userId}")
+    fun delete(@PathVariable userId: Long) {
+        return this.service.delete(userId)
     }
 }
