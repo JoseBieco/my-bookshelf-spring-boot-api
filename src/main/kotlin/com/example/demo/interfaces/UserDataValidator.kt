4b75e824deadc5bc7fa8e5b5f34c3df465260021
Jose Bieco <jo_bieco@hotmail.com>
2021-11-08 09:21:27 -0300

    feat(demo/interfaces/UserDataValidator.kt): interface to validate user information

--- src/main/kotlin/com/example/demo/interfaces/UserDataValidator.kt
+++ src/main/kotlin/com/example/demo/interfaces/UserDataValidator.kt
@@ -1,4 +1,18 @@
 package com.example.demo.interfaces
 
+/**
+ * Interface that will receive all user data validators
+ */
 interface UserDataValidator {
+
+    /**
+     * @param data String
+     * @return True if the data is not empty, and false if it's null or empty
+     */
+    fun notEmpty(data: String?): Boolean {
+        if (data != null) {
+            return data.trim().isNotEmpty()
+        }
+        return false
+    }
 }
