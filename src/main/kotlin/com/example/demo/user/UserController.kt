2ab25ba9bb81661fecec739e03d3ca9a5ff1c268
Jose Bieco <jo_bieco@hotmail.com>
2021-11-18 15:26:55 -0300

    feat(demo/user/UserController.kt): implemented jwt on login and added logout

--- src/main/kotlin/com/example/demo/user/UserController.kt
+++ src/main/kotlin/com/example/demo/user/UserController.kt
@@ -3,8 +3,11 @@ package com.example.demo.user
 import com.example.demo.user.dtos.LoginDto
 import com.example.demo.user.dtos.RegisterUserDto
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletResponse
+import com.example.demo.util.Message
 
 @RestController
 @RequestMapping("auth")
@@ -16,6 +19,8 @@ class UserController(
      *  Routes:
      *      Login -> /auth/login, method: POST
      *      Register -> /auth/register, method: POST
+     *      GetUserFromToken -> /auth/user, method: GET
+     *      Logout -> /auth/logout, method: POST
      *  Basic:
      *      Delete -> /auth, method: DELETE
      *      Update -> /auth/{userId}, method: PUT { Name, Password, Email?; }
@@ -31,12 +36,24 @@ class UserController(
     @RequestMapping("/login")
     fun login(@RequestBody login: LoginDto, response: HttpServletResponse): User {
         return this.service.login(login, response)
-        // TODO: Create function to generate jwt token
     }
 
     @DeleteMapping
     @RequestMapping("/{userId}")
     fun delete(@PathVariable userId: Long) {
         return this.service.delete(userId)
+    }
+
+    @GetMapping("/user")
+    fun getUserFromToken(@CookieValue("token") token : String?): User {
+        return this.service.getUserFromToken(token);
+    }
+
+    @PostMapping("/logout")
+    fun logout(response: HttpServletResponse): ResponseEntity<Any> {
+        val cookie = Cookie("token", "")
+        cookie.maxAge = 0
+        response.addCookie(cookie)
+        return ResponseEntity.ok(Message("Logged off!"))
     }
 }
