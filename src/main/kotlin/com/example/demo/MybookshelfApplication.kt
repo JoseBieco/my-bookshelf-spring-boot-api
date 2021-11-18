34be8ce62d62ddff3dfd5938083c7eefe76a5ad7
Jose Bieco <jo_bieco@hotmail.com>
2021-11-18 11:11:58 -0300

    feat(demo/MybookshelfApplication.kt): exclude SecurityAutoConfiguration

--- src/main/kotlin/com/example/demo/MybookshelfApplication.kt
+++ src/main/kotlin/com/example/demo/MybookshelfApplication.kt
@@ -1,9 +1,10 @@
 package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class MybookshelfApplication
 
 fun main(args: Array<String>) {
