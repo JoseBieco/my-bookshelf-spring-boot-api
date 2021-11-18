b11cac88bae90b308fc2265ef1d9d20f54a47b4a
Jose Bieco <jo_bieco@hotmail.com>
2021-11-18 11:12:48 -0300

    feat(demo/configuration/AppConfig.kt): CORS configuration

--- src/main/kotlin/com/example/demo/configuration/AppConfig.kt
+++ src/main/kotlin/com/example/demo/configuration/AppConfig.kt
@@ -4,9 +4,19 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class AppConfig {
+@EnableWebMvc
+class AppConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+       registry.addMapping("/**")
+           .allowedOrigins()
+           .allowCredentials(true)
+    }
 
     @Bean
     fun encoder(): PasswordEncoder? {
