6147d22419fc406b2cb9a501895f44c6095b6ff1
JoseBieco <jo_bieco@hotmail.com>
2021-12-03 10:27:42 -0300

    feat(demo/configuration/CORSConfig): set allowed origins

--- src/main/kotlin/com/example/demo/configuration/CORSConfig.kt
+++ src/main/kotlin/com/example/demo/configuration/CORSConfig.kt
@@ -1,26 +1,17 @@
 package com.example.demo.configuration
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-class AppConfig: WebMvcConfigurer {
+class CORSConfig: WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
        registry.addMapping("/**")
            .allowedOrigins("http://localhost:8081", "http://localhost:8080")
            .allowCredentials(true)
-    }
-
-    @Bean
-    fun encoder(): PasswordEncoder? {
-        return BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.`$2A`)
     }
 }
