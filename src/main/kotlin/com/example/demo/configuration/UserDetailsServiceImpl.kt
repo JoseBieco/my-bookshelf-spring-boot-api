916d9ad22ca9346e2e470accf122aa60278adc51
JoseBieco <jo_bieco@hotmail.com>
2021-12-03 10:34:39 -0300

    feat(demo/configuration/UserDetailsServiceImpl): implemented UserDetailsService, validating by database users

--- src/main/kotlin/com/example/demo/configuration/UserDetailsServiceImpl.kt
+++ src/main/kotlin/com/example/demo/configuration/UserDetailsServiceImpl.kt
@@ -1,13 +1,36 @@
 package com.example.demo.configuration
 
+import com.example.demo.user.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
-class UserDetailServiceImpl: UserDetailsService {
+class UserDetailsServiceImpl(
+    @Autowired
+    val db: UserRepository
+): UserDetailsService {
+
+
+
+    @Autowired
+    lateinit var passwordEncoder: PasswordEncoder
 
     override fun loadUserByUsername(login: String): UserDetails {
-        TODO("Not yet implemented")
+        val user =  db.getByEmail(login)
+            .orElseThrow { UsernameNotFoundException("User not found!") }
+
+        val roles = if (user.isAdmin) arrayOf("ADMIN", "USER") else arrayOf("USER")
+
+        return User
+                .builder()
+                .username(user.email)
+                .password(user.password)
+                .roles(*roles)
+                .build()
     }
 }
