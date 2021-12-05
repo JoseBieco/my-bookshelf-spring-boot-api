e8d05d3013cfea1dbcf5e0f54ab8dc9730a99888
JoseBieco <jo_bieco@hotmail.com>
2021-12-05 17:02:40 -0300

    feat(demo/configuration/WebSecurityConfig): added jwtFilter

--- src/main/kotlin/com/example/demo/configuration/WebSecurityConfig.kt
+++ src/main/kotlin/com/example/demo/configuration/WebSecurityConfig.kt
@@ -1,5 +1,7 @@
 package com.example.demo.configuration
 
+import com.example.demo.configuration.jwt.JwtAuthFilter
+import com.example.demo.configuration.jwt.JwtService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpMethod
@@ -7,18 +9,30 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @EnableWebSecurity
 class WebSecurityConfig(
-    @Autowired
-    val userDetailService: UserDetailsServiceImpl
 ): WebSecurityConfigurerAdapter() {
+
+    @Autowired
+    lateinit var userDetailService: UserDetailsServiceImpl
+
+    @Autowired
+    lateinit var jwtService: JwtService
 
     @Bean
     fun encoder(): PasswordEncoder {
         return BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.`$2A`)
+    }
+
+    @Bean
+    fun jwtFilter(): OncePerRequestFilter {
+        return JwtAuthFilter(jwtService, userDetailService)
     }
 
     override fun configure(auth: AuthenticationManagerBuilder) {
@@ -31,13 +45,20 @@ class WebSecurityConfig(
         http
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/api/**").hasAnyRole("ADMIN", "USER")
-            .antMatchers(HttpMethod.DELETE, "/auth/**").hasRole("ADMIN")
-            .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
-            .antMatchers("/auth/logout").authenticated()
-            .anyRequest().authenticated()
+                .antMatchers("/api/**")
+                    .hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.DELETE, "/auth/**")
+                    .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/auth/**")
+                    .permitAll()
+                .antMatchers("/auth/logout")
+                    .authenticated()
+            .anyRequest()
+                .authenticated()
             .and()
-            .httpBasic()
-
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter::class.java)
     }
 }
