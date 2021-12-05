bffdd4603cd15ac1f3b6603f49f5c4750e06e629
JoseBieco <jo_bieco@hotmail.com>
2021-12-05 11:25:42 -0300

    refactor(demo/user/UserRepository): removed unused methods and added documentation

--- src/main/kotlin/com/example/demo/user/UserRepository.kt
+++ src/main/kotlin/com/example/demo/user/UserRepository.kt
@@ -7,15 +7,13 @@ import java.util.*
 interface UserRepository: JpaRepository<User, Long> {
 
     // Get the count for how many times the email is registered
+    /**
+     * Get the count for how many times the email is registered
+     * @param email String
+     * @return number representing how many times the email is registered
+     */
     @Query("SELECT COUNT(*) AS Same FROM users WHERE email LIKE CONCAT('%', :email, '%')", nativeQuery = true)
     fun searchEmail(email: String?): Long
-
-    /**
-     * @param email String
-     * @return User based on email
-     */
-    @Query("SELECT * FROM users WHERE email LIKE :email", nativeQuery = true)
-    fun getByEmail1(email: String?): User
 
     fun getByEmail(email: String): Optional<User>
 }
