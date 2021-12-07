54ab1099adfa9968e9e8c56fab28127e1660d5be
JoseBieco <jo_bieco@hotmail.com>
2021-12-07 09:16:14 -0300

    feat(demo/book/BookRepository): added getByUserId

--- src/main/kotlin/com/example/demo/book/BookRepository.kt
+++ src/main/kotlin/com/example/demo/book/BookRepository.kt
@@ -11,4 +11,12 @@ interface BookRepository: JpaRepository<Book, Long>{
 
     @Query("SELECT a FROM Book a ")
     fun find(pageable: Pageable): Page<Book>
+
+    /**
+     * Get all books related to user id
+     * @param userId Long
+     * @param pageable Pageable
+     * @return Page of books related to passed user id
+     */
+    fun getByUserId(userId: Long, pageable: Pageable): Page<Book>
 }
