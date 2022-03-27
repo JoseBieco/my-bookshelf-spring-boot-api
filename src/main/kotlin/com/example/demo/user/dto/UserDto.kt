68e79b08ff8f73cacd63ab74e73d0551270635f3
JoseBieco <jo_bieco@hotmail.com>
2022-03-26 23:04:08 -0300

    refactor(user/dto/UserDto): validation of basic user dto

--- src/main/kotlin/com/example/demo/user/dto/UserDto.kt
+++ src/main/kotlin/com/example/demo/user/dto/UserDto.kt
@@ -1,4 +1,12 @@
 package com.example.demo.user.dto
 
-class UserDto {
-}
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+data class UserDto (
+    @field:NotBlank(message = "Name field cannot be blank")
+    @field:NotNull(message = "Name field cannot be null.")
+    @field:Size(min = 2, message = "The name size can't be lower than 2 characters.")
+    var name: String,
+)
