3e264024bd0756cdb49ce2c36f5152d529b54e4e
JoseBieco <jo_bieco@hotmail.com>
2022-03-26 23:06:00 -0300

    feat(auth/dto/RegosterDto): created register dto

--- src/main/kotlin/com/example/demo/auth/dto/RegisterDto.kt
+++ src/main/kotlin/com/example/demo/auth/dto/RegisterDto.kt
@@ -1,4 +1,21 @@
 package com.example.demo.auth.dto
 
-class RegisterDto {
-}
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+data class RegisterDto (
+    @field:NotBlank(message = "Name field cannot be blank")
+    @field:NotNull(message = "Name field cannot be null.")
+    @field:Size(min = 2, message = "The name size can't be lower than 2 characters.")
+    var name: String,
+
+    @field:Email(message = "The email must be valid.")
+    var email: String,
+
+    @field:NotBlank(message = "Password field cannot be blank")
+    @field:NotNull(message = "Password field cannot be null.")
+    @field:Size(min = 2, message = "The password size can't be lower than 2 characters.")
+    var password: String
+)
