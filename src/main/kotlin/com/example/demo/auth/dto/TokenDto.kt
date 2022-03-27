2bf9cf341e4993ee2021f91844396a921cc0f7ef
JoseBieco <jo_bieco@hotmail.com>
2022-03-26 23:09:49 -0300

    refactor(auth/dto/TokenDto): create token dto

--- src/main/kotlin/com/example/demo/auth/dto/TokenDto.kt
+++ src/main/kotlin/com/example/demo/auth/dto/TokenDto.kt
@@ -1,4 +1,10 @@
 package com.example.demo.auth.dto
 
-class TokenDto {
-}
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+data class TokenDto(
+    @field:NotBlank(message = "The token cannot be blank")
+    @field:NotNull(message = "The token cannot be null.")
+    val token: String
+)
