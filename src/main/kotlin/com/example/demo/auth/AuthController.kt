297f3f43bf2814e323af9d6716d986b3a6a883e5
JoseBieco <jo_bieco@hotmail.com>
2022-03-26 23:08:02 -0300

    refactor(auth/AuthController): refactored all the controller routes and add user remove route and change password route

--- src/main/kotlin/com/example/demo/auth/AuthController.kt
+++ src/main/kotlin/com/example/demo/auth/AuthController.kt
@@ -1,4 +1,73 @@
 package com.example.demo.auth
 
-class AuthController {
+import com.example.demo.auth.dto.*
+import com.example.demo.user.User
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("auth")
+class AuthController(
+    @Autowired
+    val service: AuthService
+) {
+    /**
+     * Register
+     * Login
+     * Logout
+     * Delete
+     */
+
+    /**
+     * Register a new user
+     * @param user RegisterDto
+     * @return User
+     */
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun register(@Validated @RequestBody user: RegisterDto): User {
+        return service.create(user)
+    }
+
+    /**
+     * Login route
+     * @param login LoginDto
+     * @return User
+     */
+    @PostMapping("/login")
+    fun login(@RequestBody login: LoginDto): LoggedDto {
+        return service.login(login)
+    }
+
+    /**
+     * Delete user from database
+     */
+    @DeleteMapping("/{userId}")
+    fun delete(@PathVariable userId: Long) {
+        return service.delete(userId)
+    }
+
+    /**
+     * Update user password
+     */
+    @PatchMapping("/{userId}")
+    fun changePassword(@PathVariable userId: Long, @RequestBody body: ChangePasswordDto) {
+        return service.updatePassword(userId, body.password)
+    }
+
+    @PostMapping("/logout")
+    fun logout(@RequestBody token: TokenDto): Any {
+        service.logout(token)
+        return ResponseEntity.ok(object { val message: String = "Logged off!" })
+    }
 }
