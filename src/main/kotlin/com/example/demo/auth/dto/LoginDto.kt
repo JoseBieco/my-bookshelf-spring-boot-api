523224f01635386ecc19da1473cbb8f508201eaa
JoseBieco <jo_bieco@hotmail.com>
2022-03-26 23:05:33 -0300

    feat(auth/dto/LoginDto): created login dto

--- src/main/kotlin/com/example/demo/auth/dto/LoginDto.kt
+++ src/main/kotlin/com/example/demo/auth/dto/LoginDto.kt
@@ -1,4 +1,16 @@
 package com.example.demo.auth.dto
 
-class LoginDto {
-}
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+data class LoginDto (
+    @field:Email(message = "The email must be valid.")
+    var email: String,
+
+    @field:NotBlank(message = "Password field cannot be blank")
+    @field:NotNull(message = "Password field cannot be null.")
+    @field:Size(min = 2, message = "The password size can't be lower than 2 characters.")
+    var password: String
+)
